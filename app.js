5e42e07c5b964647bec0a0c7f275861e646c4daf
theotshebesebe <137148933+theotshebesebe@users.noreply.github.com>
2023-06-20 17:53:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,72 @@
 function putTodo(todo) {
-    // implement your code here
-    console.log("calling putTodo");
-    console.log(todo);
+  const url = window.location.href + 'api/todo/' + todo.id; // Update the URL based on your endpoint
+
+  fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(todo),
+  })
+    .then(response => {
+      if (response.ok) {
+        console.log('Todo updated successfully');
+        // Handle success, if needed
+      } else {
+        console.log('Failed to update the todo');
+        // Handle error, if needed
+      }
+    })
+    .catch(error => {
+      console.log('Error occurred while updating the todo:', error);
+      // Handle error, if needed
+    });
 }
 
 function postTodo(todo) {
-    // implement your code here
-    console.log("calling postTodo");
-    console.log(todo);
+  const url = window.location.href + 'api/todo'; // Update the URL based on your endpoint
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(todo),
+  })
+    .then(response => {
+      if (response.ok) {
+        console.log('New todo created successfully');
+        // Handle success, if needed
+      } else {
+        console.log('Failed to create the new todo');
+        // Handle error, if needed
+      }
+    })
+    .catch(error => {
+      console.log('Error occurred while creating the new todo:', error);
+      // Handle error, if needed
+    });
 }
 
 function deleteTodo(todo) {
-    // implement your code here
-    console.log("calling deleteTodo");
-    console.log(todo);
+  const url = `${window.location.href}api/todo/${todo.id}`; // Update the URL based on your endpoint and todo ID
+
+  fetch(url, {
+    method: 'DELETE',
+  })
+    .then(response => {
+      if (response.ok) {
+        console.log('Todo deleted successfully');
+        // Handle success, if needed
+      } else {
+        console.log('Failed to delete the todo');
+        // Handle error, if needed
+      }
+    })
+    .catch(error => {
+      console.log('Error occurred while deleting the todo:', error);
+      // Handle error, if needed
+    });
 }
 
 // example using the FETCH API to do a GET request
