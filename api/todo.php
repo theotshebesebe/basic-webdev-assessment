70a9622d21c24b28edad2f1d43a153c7d5eb16c6
theotshebesebe <137148933+theotshebesebe@users.noreply.github.com>
2023-06-20 17:50:52 +0200

    Update todo.php

--- api/todo.php
+++ api/todo.php
@@ -1,7 +1,7 @@
 <?php
 try {
     require_once("todo.controller.php");
-    
+   
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $path = explode( '/', $uri);
     $requestType = $_SERVER['REQUEST_METHOD'];
@@ -9,7 +9,7 @@ try {
     $pathCount = count($path);
 
     $controller = new TodoController();
-    
+   
     switch($requestType) {
         case 'GET':
             if ($path[$pathCount - 2] == 'todo' && isset($path[$pathCount - 1]) && strlen($path[$pathCount - 1])) {
@@ -26,15 +26,58 @@ try {
                 die(json_encode($controller->loadAll()));
             }
             break;
-        case 'POST':
-            //implement your code here
-            break;
-        case 'PUT':
-            //implement your code here
-            break;
-        case 'DELETE':
-            //implement your code here
-            break;
+
+       case 'POST':
+    $data = json_decode($body);
+    if ($data) {
+        $newTodo = new Todo($data->id, $data->title, $data->description, $data->done);
+        $success = $controller->create($newTodo);
+        if ($success) {
+            http_response_code(201);
+            die();
+        }
+    }
+    http_response_code(400);
+    die();
+    break;
+       
+case 'PUT':
+    if ($path[$pathCount - 2] == 'todo' && isset($path[$pathCount - 1]) && strlen($path[$pathCount - 1])) {
+        $id = $path[$pathCount - 1];
+        $data = json_decode($body);
+        if ($data) {
+            $existingTodo = $controller->load($id);
+            if ($existingTodo) {
+                $existingTodo->title = $data->title;
+                $existingTodo->description = $data->description;
+                $existingTodo->done = $data->done;
+                $success = $controller->update($id, $existingTodo);
+                if ($success) {
+                    http_response_code(200);
+                    die();
+                }
+            }
+        }
+    }
+    http_response_code(400);
+    die();
+    break;
+
+       case 'DELETE':
+    if ($path[$pathCount - 2] == 'todo' && isset($path[$pathCount - 1]) && strlen($path[$pathCount - 1])) {
+        $id = $path[$pathCount - 1];
+        $existingTodo = $controller->load($id);
+        if ($existingTodo) {
+            $success = $controller->delete($id);
+            if ($success) {
+                http_response_code(204);
+                die();
+            }
+        }
+    }
+    http_response_code(400);
+    die();
+    break;
         default:
             http_response_code(501);
             die();
